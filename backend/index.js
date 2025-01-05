62c5f9da249c0c8e0c7e6662e7609de388376997
AmirTanweer <amirtanweer0@gmail.com>
2025-01-05 09:28:00 +0530

    User model created

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./db");
 const port=5000;
 
+app.use(cors());
+app.use(express.json());
+
+connectDB();
 app.get("/", (req, res) => {
     res.send("Hello");
 })
